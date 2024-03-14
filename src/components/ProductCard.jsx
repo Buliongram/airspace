18993cb9fd2e87b61c7d38256734334c68275a95
David <bulliongram@gmail.com>
2024-03-14 10:47:07 +0100

    Commence airspace app creation; Add the packages; create the navLinks; create the route and the pages thereon; Design both the Home and the Contact Page and also create the FeaturedSlide slider and productCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { TbMapPinPin } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { GiBed } from "react-icons/gi";
+import { MdBathtub, MdOutlineBathtub } from "react-icons/md";
+import { PiToilet } from "react-icons/pi";
+
+export default function ProductCard({
+  id,
+  image,
+  title,
+  price,
+  address,
+  bathroom,
+  bedroom,
+  restroom,
+}) {
+  return (
+    <Link
+      to={`/listing/${id}`}
+      className="rounded-md overflow-hidden min-h-44 md:min-h-56 hover:-translate-y-1 hover:shad group"
+    >
+      <div className="h-32 w-full overflow-hidden relative group">
+        <div className="bg-slate-700 group-hover:bg-orange-500  py-1 px-4 w-32 rounded-2xl text-sm sm:text-sm text-white text-center absolute z-10 top-4 left-3">
+          &#8358;{price.toLocaleString()}
+        </div>
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <div className="flex flex-col justify-between px-4 pb-4 pt-2">
+        <h5 className="text-slate-700 text-base font-semibold sm:text-lg text-justify leading-tight">
+          {title}
+        </h5>
+        <p className="flex items-center font-medium text-xs  sm:text-sm opacity-70 text-slate-600">
+          <TbMapPinPin className="mr-1 text-inherit" />
+          {address}
+        </p>
+      </div>
+      <div className="flex rounded-full items-center justify-between p-4 md:p-4 bg-white group-hover:bg-orange-500 text-slate-700 group-hover:text-white">
+        <div className="flex items-center gap-[.15rem] text-[.55rem] xl:text-xs md:gap-2">
+          <GiBed />
+          <p className="opacity-70">
+            {bedroom} bedroom{bedroom > 1 ? "s" : ""}
+          </p>
+        </div>
+        <div className="flex items-center gap-[.15rem] text-[.55rem] xl:text-xs md:gap-2">
+          <MdBathtub />
+          <p className="opacity-70">
+            {bathroom} bathroom{bathroom > 1 ? "s" : ""}
+          </p>
+        </div>
+        <div className="flex items-center gap-[.15rem] text-[.55rem] xl:text-xs md:gap-2">
+          <PiToilet />
+          <p className="opacity-70">
+            {restroom} restroom{restroom > 1 ? "s" : ""}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
